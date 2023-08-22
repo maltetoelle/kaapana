4284edc8eaba5be5ea774f5102612a57a427ab77
Halvar Kelm <halvar.kelm@gmail.com>
2023-08-22 13:07:46 +0200

    Change landingpage to floto

--- services/utils/base-landing-page/files/kaapana_app/src/components/WelcomeViews/KaapanaWelcome.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/WelcomeViews/KaapanaWelcome.vue
@@ -7,28 +7,22 @@
           v-layout
             v-flex(xs12, py-1)
               div(style={'margin': '9px', 'margin-top': '10px'})
-                img(src='../../assets/img/kaapana.png', alt='Vuetify', width='35%')
+                img(src='../../assets/img/floto.png', alt='Vuetify', width='45%')
     v-flex.text-xs-left(sm4 d-flex)
       v-card(width='100%')
         v-card-text.text-xs-left()
           div
             h2 Welcome {{ currentUser.username }}!
-            p Join us on Slack or drop us an email!
+            p Drop us an email!
             hr.my-2
         v-card-text
           v-layout(row='', wrap='')
-            v-flex( py-1)
-              v-card(href="https://join.slack.com/t/kaapana/shared_invite/zt-hilvek0w-ucabihas~jn9PDAM0O3gVQ", target="_blank" elevation=0)
-                v-card-title.justify-center
-                  v-icon(x-large) mdi-slack
-                v-card-text
-                  span Slack
             v-flex(py-1)
-              v-card(href="mailto:kaapana@dkfz.de?subject=kaapana%20Support%20Question" elevation=0)
+              v-card(href="mailto:malte.toelle@med.uni-heidelberg.de?subject=floto%20Support%20Question" elevation=0)
                 v-card-title.justify-center
                   v-icon(x-large) mdi-email
                 v-card-text
-                  span kaapana@dkfz.de
+                  span malte.toelle@med.uni-heidelberg.de
 </template>
 
 <script>
